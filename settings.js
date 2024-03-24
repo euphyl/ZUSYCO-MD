cca79f3e48170ac4ed9fbe4578c0148d8bc5f23d
euphyl <164659766+euphyl@users.noreply.github.com>
2024-03-24 14:55:02 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,10 +16,10 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94715166712",
-  SUDO_NUMBERS : "94715166712,94719574492",
-  OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
-  BOT_NAME : "ＺＵＳＹＣＯ",
+  OWNER_NUMBER: "60149881595",
+  SUDO_NUMBERS : "60149881595,60198460343",
+  OWNER_NAME: "『 Euphyx 』",
+  BOT_NAME : "E U P H Y X",
   ZUSYCO_FILE : "ZUSYCO-QR",
   LOGOS: {
     ALIVE: 'https://i.ibb.co/YTWznr9/ZUSYCO-ALIVE-IMAGE-LOWQ.jpg',
